b55fb7377b6de49a89ff56bff1e754d11e59db61
Joseph A <user@example.com>
2019-02-24 12:29:35 +0000

    Added logic to execute the commands

--- app/src/main/java/com/example/marsrobot/PlanetEngine.java
+++ app/src/main/java/com/example/marsrobot/PlanetEngine.java
@@ -6,7 +6,32 @@ public class PlanetEngine {
     @Nullable
     private Robot currentRobot = null;
 
+
+
     public void initRobot(int startX, int startY, Direction direction) {
         currentRobot = new Robot(direction, startX, startY);
     }
+
+    public String processCommandsAndGetFinalState(String input) {
+        char[] commands = input.toCharArray();
+        if (currentRobot == null) {
+            throw new IllegalStateException("Please initialise Robot before using it");
+        }
+        for (char command  : commands) {
+            switch (command) {
+                case 'L':
+                    currentRobot.addCommand(new TurnLeft());
+                    break;
+                case 'R':
+                    currentRobot.addCommand(new TurnRight());
+                    break;
+                case 'F':
+                    currentRobot.addCommand(new GoForward());
+                    break;
+            }
+        }
+        currentRobot.executeCommands();
+        return currentRobot.getFinalState();
+    }
+
 }
--- app/src/main/java/com/example/marsrobot/Robot.java
+++ app/src/main/java/com/example/marsrobot/Robot.java
@@ -1,14 +1,21 @@
 package com.example.marsrobot;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Robot {
     private Direction currentDirection;
     private int xPos;
     private int yPos;
+    private final Queue<Command> commands;
 
     public Robot(Direction currentDirection, int xPos, int yPos) {
         this.currentDirection = currentDirection;
         this.xPos = xPos;
         this.yPos = yPos;
+        commands = new LinkedList<>();
     }
 
     public Direction getCurrentDirection() {
@@ -33,5 +40,19 @@ public class Robot {
 
     public void setyPos(int yPos) {
         this.yPos = yPos;
+    }
+
+    public void addCommand(Command command) {
+        commands.add(command);
+    }
+
+    public void executeCommands() {
+        for (Command command : commands) {
+            command.execute(this);
+        }
+    }
+
+    public String getFinalState() {
+        return String.valueOf(xPos) + " " + String.valueOf(yPos) + " " + currentDirection.toString();
     }
 }
